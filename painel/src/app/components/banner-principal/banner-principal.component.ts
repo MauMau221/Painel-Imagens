59965245199972f40477b7ad6ed2244ddd733bab
MauMau221 <mauriciobgsp@gmail.com>
2025-06-22 13:36:23 -0300

    ajuste top-bar

--- painel/src/app/components/banner-principal/banner-principal.component.ts
+++ painel/src/app/components/banner-principal/banner-principal.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-banner-principal',
-  imports: [MatIconModule, LogoComponent],
+  imports: [MatIconModule],
   templateUrl: './banner-principal.component.html',
   styleUrl: './banner-principal.component.css'
 })
--- painel/src/app/components/top-bar/top-bar.component.ts
+++ painel/src/app/components/top-bar/top-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [MatIconModule],
+  imports: [MatIconModule, LogoComponent],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css'
 })
