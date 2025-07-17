4600b1cb049c255bde4345455edfe8d717454ab3
MauMau221 <mauriciobgsp@gmail.com>
2025-07-17 10:37:56 -0300

    problema local

--- painel/src/environments/environment.prod.ts
+++ painel/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://192.168.0.105:3000'
+  backendUrl: 'http://192.168.1.30:3000'
 }; 
--- painel/src/environments/environment.ts
+++ painel/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://192.168.0.105:3000'
+  backendUrl: 'http://192.168.1.30:3000'
 }; 
