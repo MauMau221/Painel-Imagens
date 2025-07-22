131b154124ecb95d7e1d4379a9d28c07dd9229a4
MauMau221 <mauriciobgsp@gmail.com>
2025-07-22 14:18:55 -0300

    teste

--- painel-backend/models/Image.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ImageSchema = new mongoose.Schema({
-  component: String,
-  description: String,
-  alt: String,
-  path: String
-});
-module.exports = mongoose.model('Image', ImageSchema); 
